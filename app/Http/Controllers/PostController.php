57c7e43164aab0f241d0000c1537ffd913a7e8a1
MaQueen <shusineko@icloud.com>
2020-11-25 11:28:00 +0900

    投稿コントローラー作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,10 +1,50 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
-    //
+    public function input()
+    {
+        return view('image.input');
+    }
+
+    public function upload(Request $request){
+        $this->validates($request,[
+            'file' => [
+                'required',
+                'file',
+                'image',
+                'mimes:jpeg,png',
+            ]
+        ]);
+
+            if($request->file('file')->isValid([])){
+            $path = $request->file->$store('public');
+
+            $file_name = basename($path);
+            $user_id = Auth::id();
+            $new_post_data = new Post();
+            $new_post_data->user_id = $user_id;
+            $new_post_data->file_name = $file_name;
+            $new_post_data->text = $text;
+
+            $new_post_data->save();
+            return redirect('/output');
+        }else{
+            return redirect()
+            ->back()
+            ->withInput()
+            ->withErrors();
+        }
+    }
+    
+    public function output(){
+        $user_id = Auth::id();
+        $user_posts = Post::whereUser_id($user_id)->get();
+        return view('post.output',['user_posts' => $user_posts]);
+    }
 }
